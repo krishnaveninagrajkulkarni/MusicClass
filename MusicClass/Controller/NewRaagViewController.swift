584d9a5a8e20b20ce23114277a772352d15534fc
krishnaveni <krishnaveni_kulkarni@yahoo.com>
2018-06-26 15:12:58 +0530

    UIView Moved with TextView Keyboard

--- MusicClass/Controller/NewRaagViewController.swift
+++ MusicClass/Controller/NewRaagViewController.swift
@@ -7,90 +7,100 @@
 //
 
 import UIKit
+import CoreData
 
-class NewRaagViewController: UIViewController,UITextFieldDelegate {
-    
-    
-    @IBOutlet weak var saptakTextField: UITextField!
-    @IBOutlet weak var sargamTextField: UITextField!
-    @IBOutlet weak var taalTextField: UITextField!
-    @IBOutlet weak var jaankariTextField: UITextField!
+class NewRaagViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
+
     
     @IBOutlet var newRaagView: UIView!
+    @IBOutlet weak var raagNameTextField: UITextField!
     
-    var kheight: CGFloat?
+    @IBOutlet weak var saptakTextView: UITextView!
+    @IBOutlet weak var taalTextView: UITextView!
+    @IBOutlet weak var sargamTextView: UITextView!
+    @IBOutlet weak var jaankariTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        raagNameTextField.delegate = self
+        saptakTextView.delegate = self
+        taalTextView.delegate = self
+        sargamTextView.delegate = self
+        jaankariTextView.delegate = self
         
-        saptakTextField.delegate = self
-        sargamTextField.delegate = self
-        taalTextField.delegate = self
-        jaankariTextField.delegate = self
-        
-        
+        //listenToKeyboardNotifications()
         
         //Create a Tap Gesture - when user clicks on View after typing is donein textfield, the keyboard should gooff
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
-        newRaagView.addGestureRecognizer(tapGesture)
-    }
-
-    @IBAction func saveButtonPressed(_ sender: UIButton) {
-        print("Data Entered = \(saptakTextField.text!)")        
+        view.addGestureRecognizer(tapGesture)
     }
     
-    //Mark - Textfield Delegate Methods
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
+    
+    //Mark - TextField n TextView Delegates Methods
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        print("i am in textViewShouldBeginEditing")
+        if textView == jaankariTextView {
+            listenToKeyboardNotifications()
+        }
         return true
     }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        if textField == jaankariTextField {
-            print("Its Jaankari text field")
+ 
+    func textViewDidEndEditing(_ textView: UITextView) {
+          print("i am in textViewDidEndEditing")
+        if textView == jaankariTextView {
+            stopListeningToKeyboardNotification()
         }
     }
-/*
-    //Mark: Configuring View for Keyboard display
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        NotificationCenter.default.addObserver(self, selector: Selector(("keyboardWillShow:")), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
 
-        NotificationCenter.default.addObserver(self, selector: Selector(("keyboardWillHide:")), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    func listenToKeyboardNotifications()
+    {
+        print("I am listening to keyboardNotification")
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func stopListeningToKeyboardNotification(){
+        print("I am NOT listening to keyboardNotification")
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    
+    @objc func keyboardWillChange(notification: NSNotification) {
+          print("notification name= \(notification.name)")
+        //calculate keyboard rect area, whic is used to calculate keyboard height
+       // let keyboardEndFrameScreenCoordinates =  (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+   
+       // let keyboardEndrame = self.view.convert(keyboardEndFrameScreenCoordinates!, to: view.window)
         
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(true)
-        NotificationCenter.default.removeObserver(self)
-    }
-    
-    func keyboardWillShow(notification: Notification) {
-        if let userInfo = notification.userInfo {
-            if let keyboardSize = (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue{
-                kheight = keyboardSize.height
-                self.animatedTextField(up: true)
-            }
-        }
-    }
-    
-    func keyboardWillHide(notification: Notification){
-        self.animatedTextField(up: false)
-     }
-    func animatedTextField(up: Bool){
-       // var movement = (up ? (-(kheight)) : (kheight))
-        UIView.animate(withDuration: 0.3) {
+        
+      let keyboardRect =  (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+        
+      
+        
+        if notification.name == NSNotification.Name.UIKeyboardWillHide {
+            print("UIView Resumes #################")
+            newRaagView.frame.origin.y = 0
+           // jaankariTextView.contentInset = UIEdgeInsets.zero
             
+            
+        }else {
+             print("UIView Moved *************")
+             newRaagView.frame.origin.y  = (-((keyboardRect?.height)!))
+            
+            //jaankariTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardEndrame.height, right: 0)
+            //jaankariTextView.scrollIndicatorInsets = jaankariTextView.contentInset
         }
     }
-    */
-    
+    //Keyboard hides on Tap Gesture
     @objc func viewTapped()
     {
-        saptakTextField.endEditing(true)
-        sargamTextField.endEditing(true)
-        taalTextField.endEditing(true)
-        jaankariTextField.endEditing(true)
+        raagNameTextField.endEditing(true)
+        saptakTextView.endEditing(true)
+        taalTextView.endEditing(true)
+        sargamTextView.endEditing(true)
+        jaankariTextView.endEditing(true)
     }
 
 }
